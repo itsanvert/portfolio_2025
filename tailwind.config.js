0be8977a58b0aba52c8712a365f229006947fb74
itsanvert <itsanvert@gmail.com>
2025-08-31 03:27:37 +0700

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,11 @@
 // tailwind.config.js
 module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  darkMode: "class", // ensures dark mode works properly
   theme: {
     extend: {
       fontFamily: {
@@ -7,4 +13,13 @@ module.exports = {
       },
     },
   },
+  safelist: [
+    "bg-black",
+    "bg-white",
+    "dark:bg-black",
+    "dark:bg-white",
+    "from-black/60",
+    "from-black/40",
+  ],
+  plugins: [],
 };
